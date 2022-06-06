8fb150503f91668a373e4cce557653771a9408bf
Konstantin <Super.Juke88@yandex.ru>
2022-06-06 17:31:52 +0400

    added adaptive for tablet and mobile resolution

--- source/js/main.js
+++ source/js/main.js
@@ -3,11 +3,40 @@ import Inputmask from "inputmask";
 
 document.addEventListener('DOMContentLoaded', () => {
   const body = document.querySelector('body');
+  const nav = document.querySelector('.nav__list');
   const btnMenu = document.querySelector('.button--burger');
   const menu = document.querySelector('.nav__wrapper');
   const main = document.querySelector('.main');
   const menuItems = document.querySelectorAll('.nav__item a');
   const inputPhone = document.querySelector('#phone-field');
+
+  const KEYCODE_TAB = 9;
+
+  function trapFocus(element) {
+    let focusableEls = element.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
+    let firstFocusableEl = focusableEls[0];
+    let lastFocusableEl = focusableEls[focusableEls.length - 1];
+
+    element.addEventListener('keydown', function(e) {
+      let isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
+
+      if (!isTabPressed) {
+        return;
+      }
+
+      if ( e.shiftKey ) /* shift + tab */ {
+        if (document.activeElement === firstFocusableEl) {
+          lastFocusableEl.focus();
+            e.preventDefault();
+          }
+        } else /* tab */ {
+        if (document.activeElement === lastFocusableEl) {
+          firstFocusableEl.focus();
+            e.preventDefault();
+          }
+        }
+    });
+  }
 
   const swiperOptions = {
     modules: [Navigation],
@@ -41,14 +70,15 @@ document.addEventListener('DOMContentLoaded', () => {
       main.classList.toggle('blur');
       body.classList.toggle('page__body--hidden');
       checkStateBtn();
+      trapFocus(nav);
     }
   };
 
   btnMenu.addEventListener('click', openOrCloseMenu);
   menuItems.forEach(item => item.addEventListener('click', openOrCloseMenu));
 
-  main.addEventListener('click', () => {
-    if (btnMenu.classList.contains('button--close')) {
+  menu.addEventListener('click', (e) => {
+    if (btnMenu.classList.contains('button--close') && e.target.classList.contains('nav__wrapper--show')) {
       openOrCloseMenu();
     }
   });
